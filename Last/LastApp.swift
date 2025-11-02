44bef91d2ddbb5237ab32b5bdf89a851bfcd5f9a
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-11-02 22:15:41 +0300

    Initial Commit

---
+++ Last/LastApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastApp.swift
+//  Last
+//
+//  Created by Abdelrahman Mohamed on 02.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct LastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LastTests/LastTests.swift
@@ -0,0 +1,17 @@
+//
+//  LastTests.swift
+//  LastTests
+//
+//  Created by Abdelrahman Mohamed on 02.11.2025.
+//
+
+import Testing
+@testable import Last
+
+struct LastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
